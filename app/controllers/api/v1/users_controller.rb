f7440e346c5592330d5eded04787f7d2304229e8
Usirusen Ikon <lexicon048@gmail.com>
2023-03-05 14:46:01 +0100

    Create Users controller

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,66 @@
+class Api::V1::UsersController < ApplicationController
+  before_action :set_user, only: %i[show edit update destroy]
+
+  # GET /users or /users.json
+  def index
+    @users = User.all
+    render json: @users
+  end
+
+  # GET /users/1 or /users/1.json
+  def show; end
+
+  # GET /users/new
+  def new
+    @user = User.new
+    render json: @user
+  end
+
+  # GET /users/1/edit
+  def edit; end
+
+  # POST /users or /users.json
+  def create
+    @user = User.new(user_params)
+
+    respond_to do |format|
+      if @user.save
+        format.json { render :show, status: :created, location: @user }
+      else
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /users/1 or /users/1.json
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /users/1 or /users/1.json
+  def destroy
+    @user.destroy
+
+    respond_to do |format|
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def user_params
+    params.require(:user).permit(:name)
+  end
+end
